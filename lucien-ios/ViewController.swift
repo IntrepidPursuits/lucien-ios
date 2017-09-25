b2d2a7fd6f01bddbd4da7368d26c56daea555771
Guled <somnibyte@gmail.com>
2017-09-25 14:17:08 -0400

    Initial Commit

---
+++ lucien-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lucien-ios
+//
+//  Created by Ahmed, Guled on 9/25/17.
+//  Copyright © 2017 Intrepid Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
