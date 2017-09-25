7da255ad9daa376ce4355d1d1c9d25f81089bfb3
artsygracie17 <gfang@intrepid.io>
2017-09-25 15:17:00 -0400

    Add RootViewController with button

---
+++ lucien-ios/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  lucien-ios
+//
+//  Created by Fang, Gracie on 9/25/17.
+//  Copyright © 2017 Intrepid Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        view.backgroundColor = UIColor.black
+    }
+}
