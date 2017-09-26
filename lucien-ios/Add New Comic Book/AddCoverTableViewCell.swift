a437d77c707bc977e5604c612863ba63cf881667
Guled <somnibyte@gmail.com>
2017-09-26 10:59:28 -0400

    Add AddComicTableViewController and AddCover Cell.

---
+++ lucien-ios/Add New Comic Book/AddCoverTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AddCoverTableViewCell.swift
+//  lucien-ios
+//
+//  Created by Ahmed, Guled on 9/26/17.
+//  Copyright © 2017 Intrepid Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class AddCoverTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addCoverButton: UIButton!
+}
