b40682fd5e0cf0e58c47ccbc9ea3f5ad7106ee49
Guled <somnibyte@gmail.com>
2017-09-26 14:52:20 -0400

    Add AddComicTableViewController and AddComicTableViewCell prototype.

--- lucien-ios/Add New Comic Book/AddCoverTableViewCell.swift
+++ lucien-ios/Add New Comic Book/AddCoverTableViewCell.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class AddCoverTableViewCell: UITableViewCell {
+
+    static let identifier = "addCoverCell"
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var addCoverButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
 }
