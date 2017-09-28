ab99972504101fcb1b83c021cb7319b196eac6a9
artsygracie17 <gfang@intrepid.io>
2017-09-28 09:47:19 -0400

    Add logout button in UI with constraints and styling

--- lucien-ios/ProfileViewController.swift
+++ lucien-ios/ProfileViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var emptyProfilePicture: UIImageView!
+    @IBOutlet weak var logoutButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         emptyProfilePicture.contentMode = .scaleAspectFit
+        logoutButton.backgroundColor = UIColor(red: 43/255, green: 45/255, blue: 66/255, alpha: 1.0)
+        logoutButton.setTitleColor(UIColor.white, for: .normal)
+        logoutButton.layer.cornerRadius = 3.0
     }
 }
