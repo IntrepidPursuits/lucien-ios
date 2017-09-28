29b21de3d5806e5ba940155f243455d7310fadaa
artsygracie17 <gfang@intrepid.io>
2017-09-28 10:23:36 -0400

    Colors coolGrey and dark set and applied to elements on profile screen

--- lucien-ios/ProfileViewController.swift
+++ lucien-ios/ProfileViewController.swift
@@ -12,12 +12,24 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var emptyProfilePicture: UIImageView!
     @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var nameTitle: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var emailTitle: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var profileTitle: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         emptyProfilePicture.contentMode = .scaleAspectFit
-        logoutButton.backgroundColor = UIColor(red: 43/255, green: 45/255, blue: 66/255, alpha: 1.0)
+        let dark = UIColor(redVal: 43, greenVal: 45, blueVal: 66, alphaVal: 1.0)
+        let coolGrey = UIColor(redVal: 143, greenVal: 144, blueVal: 154, alphaVal: 1.0)
+        logoutButton.backgroundColor = dark
         logoutButton.setTitleColor(UIColor.white, for: .normal)
         logoutButton.layer.cornerRadius = 3.0
+        nameTitle.textColor = coolGrey
+        emailTitle.textColor = coolGrey
+        name.textColor = dark
+        email.textColor = dark
+        profileTitle.textColor = dark
     }
 }
