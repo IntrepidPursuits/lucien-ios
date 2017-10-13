90dd4b5a5ee4234aa924e78ffdec8af0d853c600
Maya Saxena <maya.saxena@gmail.com>
2017-10-13 10:32:13 -0400

    Add AddComicViewModel WIP

---
+++ lucien-ios/AddNewComicBook/AddComicViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddComicViewModel.swift
+//  lucien-ios
+//
+//  Created by Maya Saxena on 10/13/17.
+//  Copyright © 2017 Intrepid Pursuits. All rights reserved.
+//
+
+import Foundation
+
+struct AddComicViewModel {
+    let seriesTitle = Variable("")
+}
